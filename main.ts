4879797306695592693bd95fbd7b4d03e0a8897a
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-05 22:00:45 +0800

    点亮小灯

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-from microbit import *
-while True:
-display.show(Image.HAPPY)
-sleep(1000)
-display.clear()
-sleep(1000)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . . . . .
+    . . . . .
+    . . . . .
+    `
+);
