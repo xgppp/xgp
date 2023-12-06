82c0041f301d74234169bb27cadabf6336880baa
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-06 21:20:29 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,3 @@
+//% weight=100 color=#0fbc11 icon="\uf1eb"
 namespace xgp {
-    
-    //% block
-    export function sayHello(): void {
-        basic.showString("Hello");
-    }
-
-    //% block
-    export function addNumbers(a: number, b: number): number {
-         return a+b;
-    }
 }
