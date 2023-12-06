3e07167b88191e22b7e77e57c947934f5aca89da
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-06 21:21:36 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 //% weight=100 color=#0fbc11 icon="\uf1eb"
-namespace xgp {
+namespace customlibrary {
+
+    //% block
+    export function sayHello(): void {
+        basic.showString("Hello");
+    }
+
+    //% block
+    export function addNumbers(a: number, b: number): number {
+        return a + b;
+    }
 }
