67a4e77d9545ba9ad81dd24373de29a52f8fb87c
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-06 17:23:45 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -36,32 +36,32 @@ enum PIN {
 }
 
 //% weight=5 color=#9900CC icon="\uf53b"
-namespace MCP23017 {
-    const MCP23017_ADDRESS = 0x20
+namespace xgp {
+    const xgp_ADDRESS = 0x20
 
-    const MCP23017_IODIRA = 0x00
-    const MCP23017_IPOLA = 0x02
-    const MCP23017_GPINTENA = 0x04
-    const MCP23017_DEFVALA = 0x06
-    const MCP23017_INTCONA = 0x08
-    const MCP23017_IOCONA = 0x0A
-    const MCP23017_GPPUA = 0x0C
-    const MCP23017_INTFA = 0x0E
-    const MCP23017_INTCAPA = 0x10
-    const MCP23017_GPIOA = 0x12
-    const MCP23017_OLATA = 0x14
+    const xgp_IODIRA = 0x00
+    const xgp_IPOLA = 0x02
+    const xgp_GPINTENA = 0x04
+    const xgp_DEFVALA = 0x06
+    const xgp_INTCONA = 0x08
+    const xgp_IOCONA = 0x0A
+    const xgp_GPPUA = 0x0C
+    const xgp_INTFA = 0x0E
+    const xgp_INTCAPA = 0x10
+    const xgp_GPIOA = 0x12
+    const xgp_OLATA = 0x14
 
-    const MCP23017_IODIRB = 0x01
-    const MCP23017_IPOLB = 0x03
-    const MCP23017_GPINTENB = 0x05
-    const MCP23017_DEFVALB = 0x07
-    const MCP23017_INTCONB = 0x09
-    const MCP23017_IOCONB = 0x0B
-    const MCP23017_GPPUB = 0x0D
-    const MCP23017_INTFB = 0x0F
-    const MCP23017_INTCAPB = 0x11
-    const MCP23017_GPIOB = 0x13
-    const MCP23017_OLATB = 0x15
+    const xgp_IODIRB = 0x01
+    const xgp_IPOLB = 0x03
+    const xgp_GPINTENB = 0x05
+    const xgp_DEFVALB = 0x07
+    const xgp_INTCONB = 0x09
+    const xgp_IOCONB = 0x0B
+    const xgp_GPPUB = 0x0D
+    const xgp_INTFB = 0x0F
+    const xgp_INTCAPB = 0x11
+    const xgp_GPIOB = 0x13
+    const xgp_OLATB = 0x15
 
     let initialized = false
 
@@ -78,23 +78,23 @@ namespace MCP23017 {
         return val;
     }
 
-    function initMCP23017(): void {
+    function initxgp(): void {
         for (let regAddr = 0; regAddr < 22; regAddr++) {
             if (regAddr == 0 || regAddr == 1) {
-                i2cwrite(MCP23017_ADDRESS, regAddr, 0xFF);
+                i2cwrite(xgp_ADDRESS, regAddr, 0xFF);
             }
             else {
-                i2cwrite(MCP23017_ADDRESS, regAddr, 0x00);
+                i2cwrite(xgp_ADDRESS, regAddr, 0x00);
             }
         }
 
         //configue all PinA output
-        i2cwrite(MCP23017_ADDRESS, MCP23017_IODIRA, 0x00);
+        i2cwrite(xgp_ADDRESS, xgp_IODIRA, 0x00);
 
         //configue all PinB input
-        i2cwrite(MCP23017_ADDRESS, MCP23017_IODIRB, 0xFF);
+        i2cwrite(xgp_ADDRESS, xgp_IODIRB, 0xFF);
         //configue all PinB pullUP
-        i2cwrite(MCP23017_ADDRESS, MCP23017_GPPUB, 0xFF);
+        i2cwrite(xgp_ADDRESS, xgp_GPPUB, 0xFF);
 
         initialized = true;
     }
@@ -102,12 +102,12 @@ namespace MCP23017 {
 
     /**
      *Read data from the register
-     * @param reg [0-21] register of mcp23017; eg: 0, 15, 23
+     * @param reg [0-21] register of xgp; eg: 0, 15, 23
     */
     //% blockId=ReadReg block="Read register |%reg| data"
     //% weight=65
     export function ReadReg(reg: REGISTER): number {
-        let val = i2cread(MCP23017_ADDRESS, reg);
+        let val = i2cread(xgp_ADDRESS, reg);
         return val;
     }
 
@@ -122,13 +122,13 @@ namespace MCP23017 {
     //% value.min=0 value.max=255
     export function WritePin(pin: PIN, value: number): void {
         if (!initialized) {
-            initMCP23017();
+            initxgp();
         }
         if (pin == 0) {
-            i2cwrite(MCP23017_ADDRESS, MCP23017_GPIOA, value);
+            i2cwrite(xgp_ADDRESS, xgp_GPIOA, value);
         }
         else {
-            i2cwrite(MCP23017_ADDRESS, MCP23017_GPIOB, value);
+            i2cwrite(xgp_ADDRESS, xgp_GPIOB, value);
         }
     }
 
@@ -140,14 +140,14 @@ namespace MCP23017 {
     //% weight=85
     export function ReadPin(pin: PIN): number {
         if (!initialized) {
-            initMCP23017();
+            initxgp();
         }
         if (pin == 0) {
-            let val = i2cread(MCP23017_ADDRESS, MCP23017_GPIOA);
+            let val = i2cread(xgp_ADDRESS, xgp_GPIOA);
             return val;
         }
         else {
-            let val = i2cread(MCP23017_ADDRESS, MCP23017_GPIOB);
+            let val = i2cread(xgp_ADDRESS, xgp_GPIOB);
             return val;
         }
     }
