46a7062d85fd0afc5b6017b1371e211c76965dcf
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-14 18:01:45 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,101 @@
-//% weight=100 color=#0fbc11 icon="\uf1eb"
-namespace xgp {
+/*
+Copyright (C): 2010-2019, Shenzhen Yahboom Tech
+modified from liusen
+load dependency
+"Tinybit": "file:../pxt-Tinybit"
+*/
 
-    //% block
-    export function sayHello(): void {
-        basic.showString("Hello");
+//% color="#006400" weight=20 icon="\uf1b9"
+namespace Tinybit {
+
+    const PWM_ADD = 0x01
+    const MOTOR = 0x02
+    const RGB = 0x01
+
+    export enum CarState {
+        //% blockId="Car_Run" block="Run"
+        Car_Run = 1,
+        //% blockId="Car_Back" block="Back"
+        Car_Back = 2,
+        //% blockId="Car_Left" block="Left"
+        Car_Left = 3,
+        //% blockId="Car_Right" block="Right"
+        Car_Right = 4,
+        //% blockId="Car_Stop" block="Stop"
+        Car_Stop = 5,
+        //% blockId="Car_SpinLeft" block="SpinLeft"
+        Car_SpinLeft = 6,
+        //% blockId="Car_SpinRight" block="SpinRight"
+        Car_SpinRight = 7
     }
 
-    //% block
-    export function addNumbers(a: number, b: number): number {
-        return a + b;
+    function setPwmMotor(mode: number, speed1: number, speed2: number): void {
+        if (mode < 0 || mode > 6)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = MOTOR;
+        switch (mode) {
+            case 0: buf[1] = 0; buf[2] = 0; buf[3] = 0; buf[4] = 0; break;              //stop
+            case 1: buf[1] = speed1; buf[2] = 0; buf[3] = speed2; buf[4] = 0; break;    //run
+            case 2: buf[1] = 0; buf[2] = speed1; buf[3] = 0; buf[4] = speed2; break;    //back
+            case 3: buf[1] = 0; buf[2] = 0; buf[3] = speed2; buf[4] = 0; break;         //left
+            case 4: buf[1] = speed1; buf[2] = 0; buf[3] = 0; buf[4] = 0; break;         //right
+            case 5: buf[1] = 0; buf[2] = speed1; buf[3] = speed2; buf[4] = 0; break;    //tleft
+            case 6: buf[1] = speed1; buf[2] = 0; buf[3] = 0; buf[4] = speed2; break;    //tright
+        }
+        pins.i2cWriteBuffer(PWM_ADD, buf);
+    }
+
+    function Car_run(speed1: number, speed2: number) {
+
+
+        setPwmMotor(1, speed1, speed2);
+    }
+
+    function Car_back(speed1: number, speed2: number) {
+
+        setPwmMotor(2, speed1, speed2);
+    }
+
+    function Car_left(speed1: number, speed2: number) {
+
+        setPwmMotor(3, speed1, speed2);
+    }
+
+    function Car_right(speed1: number, speed2: number) {
+
+        setPwmMotor(4, speed1, speed2);
+    }
+
+    function Car_stop() {
+
+        setPwmMotor(0, 0, 0);
+    }
+
+    function Car_spinleft(speed1: number, speed2: number) {
+
+        setPwmMotor(5, speed1, speed2);
+    }
+
+    function Car_spinright(speed1: number, speed2: number) {
+
+        setPwmMotor(6, speed1, speed2);
+    }
+    //% blockId=Tinybit_CarCtrl block="CarCtrl|%index"
+    //% weight=93
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrl(index: CarState): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(255, 255); break;
+            case CarState.Car_Back: Car_back(255, 255); break;
+            case CarState.Car_Left: Car_left(255, 255); break;
+            case CarState.Car_Right: Car_right(255, 255); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(255, 255); break;
+            case CarState.Car_SpinRight: Car_spinright(255, 255); break;
+        }
     }
 }
