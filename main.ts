d8dfc98a8199cbfc75520fa189df4439b4291afc
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-19 19:52:36 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -283,6 +283,7 @@ namespace microbit_car {
     let lineFollowPin1: AnalogPin;
     let lineFollowPin2: AnalogPin;
     //% weight=92 blockId=lineFollowSensor_init  block="Initialize lineFollowSensor|port %port"
+    //% subcategory=lineFollowSensor
     export function lineFollowSensor_init(port: startbit_lineFollowPort) {
         switch (port) {
             case startbit_lineFollowPort.port1:
@@ -309,6 +310,7 @@ namespace microbit_car {
     }
 
     //% weight=86 blockId=lineFollow_iic_init  block="Initialize lineFollow iic|port %port"
+    //% subcategory=lineFollowSensor
     export function lineFollow_iic_init(port: startbit_iic) {
         switch (port) {
             case startbit_iic.port3:
@@ -324,7 +326,7 @@ namespace microbit_car {
 * Get the condition of the line follower sensor
 */
     //% weight=96 blockId=startbit_readLineFollowerStatus block="Line follower status|%status"
-    //% subcategory=Sensor
+    //% subcategory=lineFollowSensor
     export function startbit_readLineFollowerStatus(status: startbit_lineFollower): boolean {
         let s1 = 0;
         let s2 = 0;
@@ -355,7 +357,7 @@ namespace microbit_car {
      * Get the line follower sensor port ad value
      */
     //% weight=89 blockId=startbit_lineSensorValue blockGap=50 block="Get line follower sensor|%sensor|ad value"
-    //% subcategory=Sensor
+    //% subcategory=lineFollowSensor
     export function startbit_lineSensorValue(sensor: startbit_LineFollowerSensor): number {
         let s1 = 0;
         let s2 = 0;
@@ -396,7 +398,7 @@ namespace microbit_car {
 
     //% weight=95 blockId=startbit_line_followers blockGap=50 block="Line follower %lineFollowerSensor in %LineColor ?"
     //% inlineInputMode=inline
-    //% subcategory=Sensor
+    //% subcategory=lineFollowSensor
     export function startbit_line_followers(lineFollowerSensor: startbit_LineFollowerSensors, LineColor: startbit_LineColor): boolean {
         pins.i2cWriteNumber(LINE_FOLLOWER_I2C_ADDR, 1, NumberFormat.UInt8BE);
         let data = pins.i2cReadNumber(LINE_FOLLOWER_I2C_ADDR, NumberFormat.UInt8BE);
