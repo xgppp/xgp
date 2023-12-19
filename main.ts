9c840f58899f3b656eb98d74dc3c6956d2737f06
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-19 17:46:26 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
- * microbit_car
+ * microbit_car
  */
-//% weight=100 color=#0fbc11 icon="\uf021"
+//% weight=100 color=#0fbc11 icon="\uf021"
 namespace microbit_carxgp {
     let _DEBUG: boolean = false
 
@@ -10,23 +10,23 @@ namespace microbit_carxgp {
     const MIN_CHIP_ADDRESS = 0x40
     const MAX_CHIP_ADDRESS = MIN_CHIP_ADDRESS + 62
     const chipResolution = 4096
-    const PrescaleReg = 0xFE //the prescale register address
-    const modeRegister1 = 0x00 // MODE1
+    const PrescaleReg = 0xFE //the prescale register address
+    const modeRegister1 = 0x00 // MODE1
     const modeRegister1Default = 0x01
-    const modeRegister2 = 0x01 // MODE2
+    const modeRegister2 = 0x01 // MODE2
     const modeRegister2Default = 0x04
-    const sleep = modeRegister1Default | 0x10; // Set sleep bit to 1
-    const wake = modeRegister1Default & 0xEF; // Set sleep bit to 0
-    const restart = wake | 0x80; // Set restart bit to 1
-    const allChannelsOnStepLowByte = 0xFA // ALL_LED_ON_L
-    const allChannelsOnStepHighByte = 0xFB // ALL_LED_ON_H
-    const allChannelsOffStepLowByte = 0xFC // ALL_LED_OFF_L
-    const allChannelsOffStepHighByte = 0xFD // ALL_LED_OFF_H
+    const sleep = modeRegister1Default | 0x10; // Set sleep bit to 1
+    const wake = modeRegister1Default & 0xEF; // Set sleep bit to 0
+    const restart = wake | 0x80; // Set restart bit to 1
+    const allChannelsOnStepLowByte = 0xFA // ALL_LED_ON_L
+    const allChannelsOnStepHighByte = 0xFB // ALL_LED_ON_H
+    const allChannelsOffStepLowByte = 0xFC // ALL_LED_OFF_L
+    const allChannelsOffStepHighByte = 0xFD // ALL_LED_OFF_H
     const PinRegDistance = 4
-    const channel0OnStepLowByte = 0x06 // LED0_ON_L
-    const channel0OnStepHighByte = 0x07 // LED0_ON_H
-    const channel0OffStepLowByte = 0x08 // LED0_OFF_L
-    const channel0OffStepHighByte = 0x09 // LED0_OFF_H
+    const channel0OnStepLowByte = 0x06 // LED0_ON_L
+    const channel0OnStepHighByte = 0x07 // LED0_ON_H
+    const channel0OffStepLowByte = 0x08 // LED0_OFF_L
+    const channel0OffStepHighByte = 0x09 // LED0_OFF_H
 
     const hexChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
@@ -80,9 +80,9 @@ namespace microbit_carxgp {
     }
 
     export enum Motor {
-        MotorLF = 1,    //FRONT
+        MotorLF = 1,    //FRONT
         MotorRF = 0,
-        MotorLR = 2,    //REAR
+        MotorLR = 2,    //REAR
         MotorRR = 3,
     }
 
@@ -97,7 +97,6 @@ namespace microbit_carxgp {
         deg315 = 315,
     }
 
-
     const pinNumber = -1
     const minOffset = 5
     const midOffset = 15
@@ -108,7 +107,6 @@ namespace microbit_carxgp {
         return (25000000 / (freq * chipResolution)) - 1;
     }
 
-
     function write(chipAddress: number, register: number, value: number): void {
         const buffer = pins.createBuffer(2)
         buffer[0] = register
@@ -117,11 +115,11 @@ namespace microbit_carxgp {
     }
 
     /**
-         * Used to setup the chip, will cause the chip to do a full reset and turn off all outputs.
-         * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
-         * @param freq [40-1000] Frequency (40-1000) in hertz to run the clock cycle at; eg: 50
+         * Used to setup the chip, will cause the chip to do a full reset and turn off all outputs.
+         * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+         * @param freq [40-1000] Frequency (40-1000) in hertz to run the clock cycle at; eg: 50
          */
-    //% block="Init addr =$chipAddress, freq =$newFreq"
+    //% block="Init addr =$chipAddress, freq =$newFreq"
     export function init(chipAddress: number = 0x40, newFreq: number = 50) {
         const buf = pins.createBuffer(2)
         freq = (newFreq > 1000 ? 1000 : (newFreq < 40 ? 40 : newFreq))
@@ -147,11 +145,11 @@ namespace microbit_carxgp {
     }
 
     /**
-     * Used to set the pulse range (0-4095) of a given pin on the PCA9685
-     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
-     * @param pinNumber The pin number (0-15) to set the pulse range on
-     * @param onStep The range offset (0-4095) to turn the signal on
-     * @param offStep The range offset (0-4095) to turn the signal off
+     * Used to set the pulse range (0-4095) of a given pin on the PCA9685
+     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
+     * @param pinNumber The pin number (0-15) to set the pulse range on
+     * @param onStep The range offset (0-4095) to turn the signal on
+     * @param offStep The range offset (0-4095) to turn the signal off
      */
     function setPinPulseRange(pinNumber: PinNum = 0, onStep: number = 0, offStep: number = 2048, chipAddress: number = 0x40): void {
         pinNumber = Math.max(0, Math.min(15, pinNumber))
@@ -160,26 +158,26 @@ namespace microbit_carxgp {
         onStep = Math.max(0, Math.min(4095, onStep))
         offStep = Math.max(0, Math.min(4095, offStep))
 
-        // Low byte of onStep
+        // Low byte of onStep
         write(chipAddress, pinOffset + channel0OnStepLowByte, onStep & 0xFF)
 
-        // High byte of onStep
+        // High byte of onStep
         write(chipAddress, pinOffset + channel0OnStepHighByte, (onStep >> 8) & 0x0F)
 
-        // Low byte of offStep
+        // Low byte of offStep
         write(chipAddress, pinOffset + channel0OffStepLowByte, offStep & 0xFF)
 
-        // High byte of offStep
+        // High byte of offStep
         write(chipAddress, pinOffset + channel0OffStepHighByte, (offStep >> 8) & 0x0F)
     }
 
     /**
-     * Used to set the duty cycle (0-100) of a given led connected to the PCA9685
-     * @param ledNumber The number (0-15) of the LED to set the duty cycle on
-     * @param dutyCycle The duty cycle (0-100) to set the LED to
+     * Used to set the duty cycle (0-100) of a given led connected to the PCA9685
+     * @param ledNumber The number (0-15) of the LED to set the duty cycle on
+     * @param dutyCycle The duty cycle (0-100) to set the LED to
      */
-    //% block
-    //% subcategory=Servo/Motor
+    //% block
+    //% subcategory=Servo/Motor
     export function setLedDutyCycle(ledNum: LEDNum = 0, dutyCycle: number): void {
         ledNum = Math.max(0, Math.min(15, ledNum))
         dutyCycle = Math.max(0, Math.min(100, dutyCycle))
@@ -188,22 +186,22 @@ namespace microbit_carxgp {
     }
 
     function degrees180ToPWM(freq: number, degrees: number, offsetStart: number, offsetEnd: number): number {
-        // Calculate the offset of the off point in the freq
+        // Calculate the offset of the off point in the freq
         offsetEnd = calcFreqOffset(freq, offsetEnd)
         offsetStart = calcFreqOffset(freq, offsetStart)
         const spread: number = offsetEnd - offsetStart
         const calcOffset: number = ((degrees * spread) / 180) + offsetStart
-        // Clamp it to the bounds
+        // Clamp it to the bounds
         return Math.max(offsetStart, Math.min(offsetEnd, calcOffset))
     }
 
     /**
-     * Used to move the given servo to the specified degrees (0-180) connected to the PCA9685
-     * @param servoNum The number (1-16) of the servo to move
-     * @param degrees The degrees (0-180) to move the servo to
+     * Used to move the given servo to the specified degrees (0-180) connected to the PCA9685
+     * @param servoNum The number (1-16) of the servo to move
+     * @param degrees The degrees (0-180) to move the servo to
      */
-    //% block
-    //% subcategory=Servo/Motor
+    //% block
+    //% subcategory=Servo/Motor
     export function setServoPosition(servoNum: PWMNum = 8, degrees: number): void {
         servoNum = Math.max(0, Math.min(15, servoNum))
         degrees = Math.max(0, Math.min(180, degrees))
@@ -211,13 +209,12 @@ namespace microbit_carxgp {
         return setPinPulseRange(<number>servoNum, 0, pwm, CHIP_ADDR)
     }
 
-
     /**
-     * Single Motor Control
-     * @param speed [-100,100] percent of fullspeed, negative is reverse
+     * Single Motor Control
+     * @param speed [-100,100] percent of fullspeed, negative is reverse
      */
-    //% block
-    //% subcategory=Servo/Motor
+    //% block
+    //% subcategory=Servo/Motor
     export function MotorControl(motor: Motor, speed: number = 0): void {
         speed = Math.max(-100, Math.min(100, speed))
         if (speed > 0) {
@@ -231,12 +228,12 @@ namespace microbit_carxgp {
     }
 
     /**
-     * Car translation use Mecanum wheel
-     * @param speed [0,100] percent of fullspeed
-     * @param degrees [0,360] direction of translation
+     * Car translation use Mecanum wheel
+     * @param speed [0,100] percent of fullspeed
+     * @param degrees [0,360] direction of translation
      */
-    //% block="Car Translation speed =$speed, degrees =$degrees"
-    //% subcategory=Servo/Motor
+    //% block="Car Translation speed =$speed, degrees =$degrees"
+    //% subcategory=Servo/Motor
     export function CarTranslation(speed: number = 0, degrees: MoveDir = MoveDir.deg0): void {
         speed = Math.max(0, Math.min(100, speed))
         degrees = Math.max(0, Math.min(360, degrees))
@@ -251,11 +248,11 @@ namespace microbit_carxgp {
     }
 
     /**
-     * Car Rotation use Mecanum wheel
-     * @param speed [-100,100] percent of fullspeed
+     * Car Rotation use Mecanum wheel
+     * @param speed [-100,100] percent of fullspeed
      */
-    //% block="Car Rotation speed =$speed"
-    //% subcategory=Servo/Motor
+    //% block="Car Rotation speed =$speed"
+    //% subcategory=Servo/Motor
     export function CarRotation(speed: number = 0): void {
         speed = Math.max(0, Math.min(100, speed))
         MotorControl(Motor.MotorLF, speed)
@@ -283,6 +280,7 @@ namespace microbit_carxgp {
     let lineFollowPin1: AnalogPin;
     let lineFollowPin2: AnalogPin;
     //% block="Initialize lineFollowSensor|port %port"
+    //% subcategory=LinePatrolSensor
     export function lineFollowSensor_init(port: startbit_lineFollowPort) {
         switch (port) {
             case startbit_lineFollowPort.port1:
@@ -300,15 +298,16 @@ namespace microbit_carxgp {
     }
 
     export enum startbit_iic {
-        //% block="Port 3"
+        //% block="Port 3"
         port3 = 0x03,
-        //% block="Port 4"
+        //% block="Port 4"
         port4 = 0x04,
-        //% block="Port 6"
+        //% block="Port 6"
         port6 = 0x06
     }
 
     //% block="Initialize lineFollow iic|port %port"
+    //% subcategory=LinePatrolSensor
     export function lineFollow_iic_init(port: startbit_iic) {
         switch (port) {
             case startbit_iic.port3:
@@ -324,6 +323,7 @@ namespace microbit_carxgp {
    * Get the condition of the line follower sensor
    */
     //% block="Line follower status|%status"
+    //% subcategory=LinePatrolSensor
     export function startbit_readLineFollowerStatus(status: startbit_lineFollower): boolean {
         let s1 = 0;
         let s2 = 0;
@@ -354,6 +354,7 @@ namespace microbit_carxgp {
      * Get the line follower sensor port ad value
      */
     //% block="Get line follower sensor|%sensor|ad value"
+    //% subcategory=LinePatrolSensor
     export function startbit_lineSensorValue(sensor: startbit_LineFollowerSensor): number {
         let s1 = 0;
         let s2 = 0;
@@ -394,6 +395,7 @@ namespace microbit_carxgp {
 
     //% block="Line follower %lineFollowerSensor in %LineColor ?"
     //% inlineInputMode=inline
+    //% subcategory=LinePatrolSensor
     export function startbit_line_followers(lineFollowerSensor: startbit_LineFollowerSensors, LineColor: startbit_LineColor): boolean {
         pins.i2cWriteNumber(LINE_FOLLOWER_I2C_ADDR, 1, NumberFormat.UInt8BE);
         let data = pins.i2cReadNumber(LINE_FOLLOWER_I2C_ADDR, NumberFormat.UInt8BE);
